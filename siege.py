1c29f277542034df5efb7b6be1e6a48d4c767f76
mozgovetsaa-ui <mozgovetsaa@gmail.com>
2026-09-17 22:37:20 +0300

    Update siege.py

--- siege.py
+++ siege.py
@@ -7,10 +7,70 @@ import sys
 
 
 def main(path: str) -> None:
-    # TODO: прочитать лог, посчитать урон, напечатать строки вида
-    # "Игрок <Имя> из гильдии <Тег> нанес <Урон> по воротам."
-    pass
+    a = open(path).readlines()[2:]
 
+    for i in a:
+        if i == "\n":
+            continue
+
+        if "[" in i:
+            i = i[i.find("[") :]
+
+        b = i.split("|")
+
+        if len(b) != 4:
+            print("Ошибка: некорректное колличество полей")
+            continue
+
+        if "[[" in b[0]:
+            b[0] = b[0].replace("[[", "[", 1).replace("]]", "]", 1)
+
+        if "[" not in b[0]:
+            guild = "not found"
+            name = b[0].strip()
+            base_damage, condition, buffs = (
+                b[1].strip().replace(",", ".").replace(" ", ""),
+                b[2].strip(),
+                b[3].strip(),
+            )
+        else:
+            guild = b[0][1 : b[0].find("]")].upper()
+            name = b[0][b[0].find("]") + 1 :].strip()
+            base_damage, condition, buffs = (
+                b[1].strip().replace(",", ".").replace(" ", ""),
+                b[2].strip(),
+                b[3].strip(),
+            )
+
+        if condition == "Active" or condition == "active":
+            condition = 1.5
+        else:
+            condition = 0.5
+
+        if buffs == "":
+            buffs = 0
+
+        if buffs == "N/A":
+            print("Ошибка: колличество баффов не указано")
+            continue
+
+        if float(buffs) < 0:
+            print("Ошибка: отрицательное колличество баффов")
+            continue
+
+        if float(base_damage) < 0:
+            print("Ошибка: отрицательный урон")
+            continue
+
+        if base_damage == "inf":
+            print("Ошибка: бесконечный урон")
+            continue
+
+        if guild == "":
+            guild = "not found"
+
+        damage = round(float(base_damage) * condition * (1 + 0.15 * float(buffs)), 2)
+        print(f"Игрок {name} из гильдии {guild.upper()} нанес {damage} по воротам.")
 
 if __name__ == "__main__":
     main(sys.argv[1] if len(sys.argv) > 1 else "siege_log.txt")
